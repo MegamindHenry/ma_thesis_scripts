f6e5ca04048f46645471bfc51b55947e8bf81d67
Xuefeng Luo <hakunamatatahenry@gmail.com>
2021-01-27 19:29:28 +0100

    unseen A

--- french_care/french_1.jl
+++ french_care/french_1.jl
@@ -18,6 +18,17 @@ cue_obj_train, cue_obj_val = JudiLing.make_cue_matrix(
   keep_sep=true
   )
 
+# create C matrixes for training datasets
+cue_obj_train1, cue_obj_val1 = JudiLing.make_combined_cue_matrix(
+  french_train,
+  french_val,
+  grams=2,
+  target_col=:Syllables,
+  tokenized=true,
+  sep_token="-",
+  keep_sep=true
+  )
+
 # retrieve dim of C
 # we set the S matrixes as the same dimensions
 n_features = size(cue_obj_train.C, 2)
@@ -54,7 +65,7 @@ Shat_val = cue_obj_val.C * F_train
 @show JudiLing.eval_SC(Shat_val, S_val, french_val, :Syllables)
 
 # here we only use a adjacency matrix as we got it from training dataset
-A_train = cue_obj_train.A
+A_train = cue_obj_train1.A
 
 # we calculate how many timestep we need for learn_paths and huo function
 max_t = JudiLing.cal_max_timestep(french_train, french_val, :Syllables)
